7b9ca6e852de74cee920951816de54bcbb6e7e36
CMWallace89 <chanel.m.wallace1@gmail.com>
2024-07-14 16:29:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 (function (window) {
+
+   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
   for (var i = 0; i < window.names.length; i++) {
     var firstLetter = window.names[i].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
